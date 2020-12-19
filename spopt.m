cf9aa6412084b4bc3c3b27cb4116add38aecc530
Bin Gao <gaobin@lsec.cc.ac.cn>
2020-12-19 18:57:03 +0100

    Update spopt.m

--- spopt.m
+++ spopt.m
@@ -68,7 +68,7 @@ if ~isfield(opts, 'record');    opts.record = 0;     end
 
 % parameters for control the linear approximation in line search,
 if ~isfield(opts, 'tau');       opts.tau  = 1e-3;    end % initial stepsize
-if ~isfield(opts, 'maxtau');    opts.maxtau  = 1e15;  end % maximal stepsize
+if ~isfield(opts, 'maxtau');    opts.maxtau  = 1e5;  end % maximal stepsize
 if ~isfield(opts, 'mintau');    opts.mintau  = 1e-15;end % nimimal stepsize
 if ~isfield(opts, 'rhols');     opts.rhols  = 1e-4;  end % linear-search condition
 if ~isfield(opts, 'eta');       opts.eta  = 0.1;     end % backtracking parameter
